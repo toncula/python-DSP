5566b8cb44a58fc0854bbd31634ce8b5a3f11650
toncula <87453682+toncula@users.noreply.github.com>
2023-11-08 20:43:21 +0800

    adding mel part

--- task2/mel.py
+++ task2/mel.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fftpack import dct
+import librosa
 
 SAMPLE_RATE = 8000 #音频文件采样率
 N_FFT = 128 #fft的N
@@ -10,9 +11,6 @@ def E_frame(f):#计算能量谱
     mag_f = np.absolute(f)
     pow_f = (1.0/N_FFT) * ((mag_f)**2)
     return pow_f 
-
-
-
 
 def main():
     dataset = np.load("data_fft_with_type.npz")
@@ -43,17 +41,28 @@ def main():
         e_frame = E_frame(fft_m[i])
         #梅尔滤波能量
         fbank_feat = np.dot(e_frame,fbank.T)
-        fbank_feat = np.where(fbank_feat == 0, np.finfo(float).eps, fbank_feat) 
+        fbank_feat = np.where(fbank_feat == 0, np.finfo(float).eps, fbank_feat) #防止0
         fbank_feat = 20 * np.log10(fbank_feat)                                
 
         # 计算mfcc系数
         mfcc_feat = dct(fbank_feat, type=2, axis=1, norm='ortho')[:, :12]
         mfcc_m.append(mfcc_feat)
         print(mfcc_feat)
+        #librosa.display.specshow(data=mfcc_feat,
+        #                     sr=SAMPLE_RATE,
+        #                    n_fft=N_FFT,
+        #                     hop_length=SAMPLE_RATE // 100,
+        #                     win_length=SAMPLE_RATE // 40,
+        #                     x_axis="s")
+        
+        #plt.colorbar(format="%d")
+        #plt.show()
+
     
     np.savez('mfcc12_result_with_type.npz',matrix1=mfcc_m,matrix2=type_m)
 
 
+
+
 if __name__ == '__main__':
     main()
-
